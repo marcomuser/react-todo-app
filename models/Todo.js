dd6880ba6ca405ca86eb2916b4351830775d7067
Marco Muser <marco.muser@posteo.de>
2020-08-17 10:17:51 +0200

    add completed field to todo-schema

--- models/Todo.js
+++ models/Todo.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   content: String,
   date: Date,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
