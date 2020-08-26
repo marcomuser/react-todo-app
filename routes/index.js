e5ee3f23b7a4819c40cb197ae8794d4d0bf774ab
Marco Muser <marco.muser@posteo.de>
2020-08-26 09:32:35 +0200

    use Express' built-in error handler instead of simple console.logs

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,35 @@ const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const todoItems = await Todo.find();
     res.status(200).json(todoItems);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
+    next(err);
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     const { content } = req.body;
     const date = Date.now();
     const todoItem = await Todo.create({ content, date });
     res.status(200).json(todoItem);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
+    next(err);
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res, next) => {
   try {
     const todoList = await Todo.findByIdAndDelete(req.params.id);
     res.status(200).json(todoList);
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
+    next(err);
   }
 });
 
