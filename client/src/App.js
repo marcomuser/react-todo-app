041c7242c9b390f4e3a5438b778cbfd878181721
Marco Muser <marco.muser@posteo.de>
2020-08-27 14:47:53 +0200

    add initial rendering logic of to-do list

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const todoList = todos.map((todo) => {
+    return <li>{todo}</li>;
+  });
+
   return (
     <div>
+      <h2>TO-DO List</h2>
+      <ul>
+        {todos.length === 0 ? <p>Add your first to-do item</p> : todoList}
+      </ul>
     </div>
   );
 }
