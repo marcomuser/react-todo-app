988dc7016a40282f232d555e7bfe52e0c180beb5
Marco Muser <marco.muser@posteo.de>
2020-08-29 17:43:40 +0200

    add frontend functionality for creating todos

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [formInput, setFormInput] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,6 +22,22 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content: formInput }),
+      });
+      const result = await response.json();
+      setTodos((prevArr) => [...prevArr, result]);
+      setFormInput("");
+    } catch (err) {
+      setIsError(true);
+    }
+  };
+
   const todoList = todos.map((todo) => {
     return <li key={todo._id}>{todo.content}</li>;
   });
@@ -28,6 +45,14 @@ function App() {
   return (
     <>
       <h2>TO-DO List</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={formInput}
+          onChange={(event) => setFormInput(event.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
       {isError && <p>Something went wrong...</p>}
       {isLoading ? <p>Loading...</p> : <ul>{todoList}</ul>}
     </>
